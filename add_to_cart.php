d1736456d72f9507369200cc6cc4791c9fab0ed3
salmi2 <92025603+salmi2@users.noreply.github.com>
2022-03-17 18:47:48 +0100

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
     include 'config.php';
-    if(!isset($_SESSION["cart"] )){
-        $_SESSION["cart"] = [];
+    if(!isset($_SESSION["cart1"] )){
+        $_SESSION["cart1"] = [];
     }
     $object = (object) ["idProduit"=>$_GET["id"], "quantite"=>$_POST["quantite"]];
-    array_push($_SESSION["cart"], $object);
+    array_push($_SESSION["cart1"], $object);
     header("location: cart.php");
 ?>
